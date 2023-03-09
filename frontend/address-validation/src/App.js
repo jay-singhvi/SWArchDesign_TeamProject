d3ec74027822f5bfc7b5a0a3ca7d9ee2c2f748f8
jen-chieh <x0930186031@gmail.com>
2023-03-08 17:30:31 -0800

    Added name for default form

--- frontend/address-validation/src/App.js
+++ frontend/address-validation/src/App.js
@@ -731,6 +731,7 @@ function DefaultForm({ setFormData }) {
             className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-2 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
             type="text"
             placeholder="Full Name"
+            name='Name'
           />
         </div>
 
